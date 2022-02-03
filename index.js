d25de4b54175e79d9a428dd9661627df3ee2b099
Timothy Lyle <itsfreezingouthere@yahoo.com>
2022-02-02 20:01:07 -0500

    commit tree ec0802353cd37173b6f817a5e7310c47bbe2fb4c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+function findMatching(driversNamesArray, driverName) {
+    return driversNamesArray.filter(driver => driver.toUpperCase() === driverName.toUpperCase())
+
+};
+
+function fuzzyMatch(driverArray, string) {
+    return driverArray.filter(driver => driver.startsWith(string))
+
+};
+
+function matchName(driverObjs, string) {
+    return driverObjs.filter(driverObjs => driverObjs.name === string)
+
+};
+
+//    return driverObjs.filter(driverObjs => driverObjs['name'] === string)
+
+//person1['lastname'] = 'Rossi';
+
+//matchName - This function takes an array of driver objects and a string as arguments. Each driver object has two properties: name and hometown. The function should return each element whose name property matches the provided string argument.
+
+
+//text.startsWith("Hello");
+
+
+  /*const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]*/
